24d50626db9bf9bb232132de98e63962e762da70
Zach <zach.j.atkinson@gmail.com>
2025-03-22 16:30:19 +0000

    An RL Policy for a robot to pick a block up

---
+++ config/UR3_Gripper/joint_pos_env_cfg.py
@@ -0,0 +1,113 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+from isaaclab.assets import RigidObjectCfg
+from isaaclab.sensors import FrameTransformerCfg
+from isaaclab.sensors.frame_transformer.frame_transformer_cfg import OffsetCfg
+from isaaclab.sim.schemas.schemas_cfg import RigidBodyPropertiesCfg
+from isaaclab.sim.spawners.from_files.from_files_cfg import UsdFileCfg
+from isaaclab.utils import configclass
+from isaaclab.utils.assets import ISAAC_NUCLEUS_DIR
+
+from isaaclab_tasks.manager_based.manipulation.lift import mdp
+from isaaclab_tasks.manager_based.manipulation.lift.lift_env_cfg import LiftEnvCfg
+
+##
+# Pre-defined configs
+##
+from isaaclab.markers.config import FRAME_MARKER_CFG  # isort: skip
+from isaaclab_assets import UR3_2F_140_CFG  # isort: skip
+
+
+@configclass
+class UR3CubeLiftEnvCfg(LiftEnvCfg):
+    def __post_init__(self):
+        # post init of parent
+        super().__post_init__()
+
+        # Set Franka as robot
+        self.scene.robot = UR3_2F_140_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")
+
+        # Set actions for the specific robot type (franka)
+        self.actions.arm_action = mdp.JointPositionActionCfg(
+            asset_name="robot", joint_names=["shoulder_pan_joint",
+            "shoulder_lift_joint",
+            "elbow_joint",
+            "wrist_1_joint",
+            "wrist_2_joint",
+            "wrist_3_joint"], scale=0.5, use_default_offset=True
+        )
+        self.actions.gripper_action = mdp.BinaryJointPositionActionCfg(
+            asset_name="robot",
+            joint_names=[
+            "finger_joint",
+      #      "right_outer_knuckle_joint"
+             ],
+
+            open_command_expr={
+            "finger_joint":0.2,
+          # "right_outer_knuckle_joint":0.2
+           
+           },
+
+            close_command_expr={
+            "finger_joint":.7,
+          #  "right_outer_knuckle_joint":.78
+           
+           },
+        )
+        # Set the body name for the end effector
+        self.commands.object_pose.body_name = "robotiq_base_link"
+
+        # Set Cube as object
+        self.scene.object = RigidObjectCfg(
+            prim_path="{ENV_REGEX_NS}/Object",
+            init_state=RigidObjectCfg.InitialStateCfg(pos=[0.0, 0, 0.00], rot=[1, 0, 0, 0]),
+            spawn=UsdFileCfg(
+                usd_path=f"{ISAAC_NUCLEUS_DIR}/Props/Blocks/DexCube/dex_cube_instanceable.usd",
+                scale=(0.65, 0.65, 0.65),
+                rigid_props=RigidBodyPropertiesCfg(
+                    solver_position_iteration_count=16,
+                    solver_velocity_iteration_count=1,
+                    max_angular_velocity=1000.0,
+                    max_linear_velocity=1000.0,
+                    max_depenetration_velocity=5.0,
+                    disable_gravity=False,
+                ),
+            ),
+        )
+
+        
+        # Listens to the required transforms
+        marker_cfg = FRAME_MARKER_CFG.copy()
+        marker_cfg.markers["frame"].scale = (0.1, 0.1, 0.1)
+        marker_cfg.prim_path = "/Visuals/FrameTransformer"
+        self.scene.ee_frame = FrameTransformerCfg(
+            prim_path="{ENV_REGEX_NS}/Robot/base_link",
+            debug_vis=False,
+            visualizer_cfg=marker_cfg,
+            target_frames=[
+                FrameTransformerCfg.FrameCfg(
+                    prim_path="{ENV_REGEX_NS}/Robot/Robotiq_2F_140_physics_edit/robotiq_base_link",
+                    name="end_effector",
+                    offset=OffsetCfg(
+                        pos=[.0, 0.0, 0.215],
+                        rot=[1, 0,0,0]
+                    ),
+                ),
+            ],
+        )
+
+
+@configclass
+class UR3CubeLiftEnvCfg_PLAY(UR3CubeLiftEnvCfg):
+    def __post_init__(self):
+        # post init of parent
+        super().__post_init__()
+        # make a smaller scene for play
+        self.scene.num_envs = 50
+        self.scene.env_spacing = 2.5
+        # disable randomization for play
+        self.observations.policy.enable_corruption = False
---
+++ universal_robots.py
@@ -0,0 +1,196 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""Configuration for the Universal Robots.
+
+The following configuration parameters are available:
+
+* :obj:`UR10_CFG`: The UR10 arm without a gripper.
+
+Reference: https://github.com/ros-industrial/universal_robot
+"""
+
+import isaaclab.sim as sim_utils
+from isaaclab.actuators import ImplicitActuatorCfg
+from isaaclab.assets.articulation import ArticulationCfg
+from isaaclab.utils.assets import ISAACLAB_NUCLEUS_DIR
+
+##
+# Configuration
+##
+
+
+UR10_CFG = ArticulationCfg(
+    spawn=sim_utils.UsdFileCfg(
+        usd_path=f"{ISAACLAB_NUCLEUS_DIR}/Robots/UniversalRobots/UR10/ur10_instanceable.usd",
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            disable_gravity=False,
+            max_depenetration_velocity=5.0,
+        ),
+        activate_contact_sensors=False,
+    ),
+    init_state=ArticulationCfg.InitialStateCfg(
+        joint_pos={
+            "shoulder_pan_joint": 0.0,
+            "shoulder_lift_joint": -1.712,
+            "elbow_joint": 1.712,
+            "wrist_1_joint": 0.0,
+            "wrist_2_joint": 0.0,
+            "wrist_3_joint": 0.0,
+        },
+    ),
+    actuators={
+        "arm": ImplicitActuatorCfg(
+            joint_names_expr=[".*"],
+            velocity_limit=100.0,
+            effort_limit=87.0,
+            stiffness=800.0,
+            damping=40.0,
+        ),
+    },
+)
+"""Configuration of UR-10 arm using implicit actuator models."""
+UR3_CFG = ArticulationCfg(
+    spawn=sim_utils.UsdFileCfg(
+        usd_path=f"https://omniverse-content-production.s3-us-west-2.amazonaws.com/Assets/Isaac/4.5/Isaac/Robots/UniversalRobots/ur3/ur3.usd",
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            disable_gravity=False,
+            max_depenetration_velocity=5.0,
+        ),
+        activate_contact_sensors=False,
+    ),
+    init_state=ArticulationCfg.InitialStateCfg(
+        joint_pos={
+            "shoulder_pan_joint": 0.0,
+            "shoulder_lift_joint": -1.65,
+            "elbow_joint": 1.6,
+            "wrist_1_joint": 1.5,
+            "wrist_2_joint": 1.5,
+            "wrist_3_joint": 1.5,
+        },
+    ),
+    actuators={
+        "arm": ImplicitActuatorCfg(
+            joint_names_expr=[".*"],
+            velocity_limit=100.0,
+            effort_limit=87.0,
+            stiffness=800.0,
+            damping=40.0,
+        ),
+    },
+)
+
+"""Configuration of UR-10 arm using implicit actuator models."""
+UR3_2F_85_CFG = ArticulationCfg(
+    spawn=sim_utils.UsdFileCfg(
+        usd_path=f"C:/Users/zach/documents/test7.usd",
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            disable_gravity=False,
+            max_depenetration_velocity=5.0,
+            
+        ),
+        
+        activate_contact_sensors=True,
+    ),
+     init_state=ArticulationCfg.InitialStateCfg(
+     joint_pos={
+            "shoulder_pan_joint": 0.0,
+            "shoulder_lift_joint": -1.65,
+            "elbow_joint": 1.6,
+            "wrist_1_joint": 1.5,
+            "wrist_2_joint": 1.5,
+            "wrist_3_joint": 1.5,
+            "left_inner_finger_knuckle_joint" : 0.000,
+            "right_inner_finger_knuckle_joint" : 0.000,
+            "left_inner_finger_joint" : 0.000,
+            "right_inner_finger_joint" : 0.000,
+           "right_outer_knuckle_joint" : 0.000,
+            "left_outer_finger_joint":0.000,
+            "right_outer_finger_joint": 0.000,
+            "finger_joint" : 0.00
+           
+        },
+     ),
+    
+    actuators={
+        "arm": ImplicitActuatorCfg(
+            joint_names_expr=["shoulder_pan_joint","shoulder_lift_joint", "elbow_joint", "wrist_1_joint", "wrist_2_joint","wrist_3_joint"],
+            velocity_limit=100.0,
+            effort_limit=87.0,
+            stiffness=800.0,
+            damping=40.0,
+        ),
+        "finger": ImplicitActuatorCfg(
+            joint_names_expr=["finger_joint", ],#["left_outer_finger_joint", "right_outer_finger_joint","left_inner_finger_knuckle_joint","right_inner_finger_knuckle_joint","left_inner_finger_joint","right_inner_finger_joint","right_outer_knuckle_joint","finger_joint" ],
+            velocity_limit = 200,
+            effort_limit=100.0,
+            stiffness=2e11,
+            damping=0,
+            friction = 1
+        ),
+        "hand": ImplicitActuatorCfg(
+            joint_names_expr=["left_outer_finger_joint", "right_outer_finger_joint","left_inner_finger_knuckle_joint", "right_inner_finger_knuckle_joint",
+            "left_inner_finger_joint",
+            "right_inner_finger_joint",
+           "right_outer_knuckle_joint", ],
+            velocity_limit = 200,
+            effort_limit=100.0,
+            stiffness=10,
+            damping=10,
+            friction = 1
+        )
+    
+    
+    },
+)
+
+"""Configuration of UR3 with gripper 2F_140_CFG using implicit actuator models."""
+UR3_2F_140_CFG = ArticulationCfg(
+    spawn=sim_utils.UsdFileCfg(
+        usd_path=f"C:/Users/zach/documents/test15.usd",  #Update file path to relevant location
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            disable_gravity=False,
+            max_depenetration_velocity=5.0,
+            
+        ),
+       
+        activate_contact_sensors=True,
+    ),
+     init_state=ArticulationCfg.InitialStateCfg(
+     joint_pos={
+            "shoulder_pan_joint": 0.0,
+            "shoulder_lift_joint": -1.45,
+            "elbow_joint": 1.15,
+            "wrist_1_joint": 4.5,
+            "wrist_2_joint": -1.5,
+            "wrist_3_joint": 0,
+            "finger_joint":0.2,
+            "right_outer_knuckle_joint":0.0,
+         
+           
+        },
+     ),
+    
+    actuators={
+        "arm": ImplicitActuatorCfg(
+            joint_names_expr=["shoulder_pan_joint","shoulder_lift_joint", "elbow_joint", "wrist_1_joint","wrist_2_joint", "wrist_3_joint"],
+            velocity_limit=1.0,
+            effort_limit=60.0,
+            stiffness=800.0,
+            damping=40.0,
+        ),
+       
+    "finger": ImplicitActuatorCfg(
+            joint_names_expr=["finger_joint"],
+            velocity_limit = 1.2,
+            effort_limit=50.0,
+            stiffness=200,
+            damping=10,
+            friction = 0.8,
+
+        ),
+
+    }
+)
